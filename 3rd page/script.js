a19b13582ca80f0be7e895e4cc3fc4decdb963f1
engmohanedelsaeed1-hue <engmohanedelsaeed1@gmail.com>
2026-05-03 15:10:11 +0300

    done

--- 3rd page/script.js
+++ 3rd page/script.js
@@ -6,11 +6,3 @@ toggle.addEventListener("click", function (e) {
   document.body.classList.toggle("dark-mode");
 });
 
-const menuToggle = document.getElementById("menuToggle");
-const navbar = document.getElementById("navbar");
-
-menuToggle.onclick = function () {
-  navbar.classList.toggle("active");
-};
-
-
