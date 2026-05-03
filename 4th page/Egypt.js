c8434c72646567ae7c79470a2446627a41e4b5c6
hanenaly740-sudo <hanenaly740@gmail.com>
2026-05-03 23:01:58 +0300

    3

--- 4th page/Egypt.js
+++ 4th page/Egypt.js
@@ -3,12 +3,3 @@ toggle.addEventListener("click", function (e) {
     e.preventDefault();
     document.body.classList.toggle("dark-mode");
 });
-function bookTicket() {
-    const toast = document.getElementById("toast");
-
-    toast.classList.add("show");
-
-    setTimeout(() => {
-        toast.classList.remove("show");
-    }, 3000);
-}
