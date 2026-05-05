de7c748d48a69c2f60c14e45217dc314f5547219
Demonsset <kenawyh5@gmail.com>
2026-05-06 00:43:23 +0300

    final palestine

--- palestine/script.js
+++ palestine/script.js
@@ -1,16 +1,33 @@
-const modeSwitch=document.getElementById('modeSwitch');
-const modeIcon=document.getElementById('modeIcon')
-const bodyElement =document.body;
-modeSwitch.addEventListener('click',(e) => {
-    bodyElement.classList.toggle('darkmode');
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = themeToggle.querySelector("i");
+const layoutToggle = document.getElementById("layoutToggle");
+const layoutIcon = layoutToggle.querySelector("i");
 
-    if (document.body.classList.contains('darkmode')) {
-        modeIcon.classList.remove('fa-sun');
-        modeIcon.classList.add('fa-moon');
+if (localStorage.getItem("darkmode") === "active") {
+    document.body.classList.add("darkmode");
+    themeIcon.classList.replace("fa-moon", "fa-sun");
+}
+
+themeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("darkmode");
+    if (document.body.classList.contains("darkmode")) {
+        themeIcon.classList.replace("fa-moon", "fa-sun");
+        localStorage.setItem("darkmode", "active");
     } else {
-        modeIcon.classList.remove('fa-moon');
-        modeIcon.classList.add('fa-sun');
+        themeIcon.classList.replace("fa-sun", "fa-moon");
+        localStorage.setItem("darkmode", null);
     }
-} );
-function showConfirmation() { 
-    alert(" Thank you! Your journey to Palestine awaits. Explore Jerusalem, Acre & more. Tickets will be reserved.") ; }
+});
+
+layoutToggle.addEventListener("click", function () {
+    document.body.classList.toggle("sidebar-layout");
+    if (document.body.classList.contains("sidebar-layout")) {
+        layoutIcon.classList.replace("fa-grip", "fa-table-list");
+    } else {
+        layoutIcon.classList.replace("fa-table-list", "fa-grip");
+    }
+});
+
+function showConfirmation() {
+    alert("Thank you! Your journey to Palestine awaits. Explore Jerusalem, Acre & more. Tickets will be reserved.");
+}
