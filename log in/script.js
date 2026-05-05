b6538b85e86af30d8f120f650de95f25bd38da20
kerolos khalil mikhaile <kerokhalil9980@gmail.com>
2026-05-05 08:22:59 -0700

    Update script.js

--- log in/script.js
+++ log in/script.js
@@ -1,23 +1,41 @@
-const modeToggle = document.querySelector('.sync-icon');
-const body = document.body;
-
-modeToggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    
-    body.classList.toggle('light-mode');
-    
-    const icon = this.querySelector('i');
-    
-    if (body.classList.contains('light-mode')) {
-        icon.classList.replace('fa-moon', 'fa-sun'); 
-        icon.style.color = '#f4b400';
-    } else {
-        icon.classList.replace('fa-sun', 'fa-moon'); 
-        icon.style.color = '#ad936d'; 
-    }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+    const modeToggle = document.querySelector('.sync-icon');
+    const body = document.body;
+
+    if (modeToggle) {
+        modeToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            body.classList.toggle('light-mode');
+            const icon = this.querySelector('i');
+            
+            if (body.classList.contains('light-mode')) {
+                icon.classList.replace('fa-moon', 'fa-sun'); 
+                icon.style.color = '#f4b400';
+            } else {
+                icon.classList.replace('fa-sun', 'fa-moon'); 
+                icon.style.color = '#ad936d'; 
+            }
+        });
+    }
+
+    const layoutToggle = document.getElementById('layoutToggle');
+    if (layoutToggle) {
+        layoutToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            console.log("Layout button clicked!"); 
+            document.body.classList.toggle('sidebar-mode');
+            
+            const icon = this.querySelector('i');
+            if (icon) {
+                if (document.body.classList.contains('sidebar-mode')) {
+                    icon.style.transform = 'rotate(90deg)'; 
+                } else {
+                    icon.style.transform = 'rotate(0deg)';
+                }
+            }
+        });
+    }
+
     const loginForm = document.getElementById('epicLogin');
     if (loginForm) {
         loginForm.addEventListener('submit', function(event) {
@@ -26,8 +44,9 @@ document.addEventListener('DOMContentLoaded', () => {
             const passwordInput = this.querySelector('input[type="password"]');
             const username = usernameInput.value.trim();
             const password = passwordInput.value;
-            const nameRegex = /^[a-zA-Z\s\u0600-\u06FF]+$/;
             
+            // Validation
+            const nameRegex = /^[a-zA-Z\s\u0600-\u06FF]+$/;
             if (!nameRegex.test(username)) {
                 alert("Please Enter The Name (Letters Only)");
                 usernameInput.focus();
