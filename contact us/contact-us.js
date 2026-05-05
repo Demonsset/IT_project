06aca13a8bc4a6ef7d47bcb762be430f275195bc
Demonsset <kenawyh5@gmail.com>
2026-05-05 23:37:45 +0300

    fixed contact us

--- contact us/contact-us.js
+++ contact us/contact-us.js
@@ -1,9 +1,24 @@
-const toggle = document.getElementById("themeToggle");
-toggle.addEventListener("click", function (e) {
-    e.preventDefault();
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = themeToggle.querySelector("i");
+const menuBtn = document.getElementById("menuToggle");
+const nav = document.querySelector(".nav");
+const parent = document.querySelector(".perant");
+
+themeToggle.addEventListener("click", function () {
     document.body.classList.toggle("darkmode");
+    if (document.body.classList.contains("darkmode")) {
+        themeIcon.classList.replace("fa-moon", "fa-sun");
+    } else {
+        themeIcon.classList.replace("fa-sun", "fa-moon");
+    }
 });
- document.querySelector(".send").onclick = function () {
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    parent.classList.toggle("menu-open");
+});
+
+document.querySelector(".send").onclick = function () {
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let number = document.getElementById("number").value.trim();
@@ -13,19 +28,12 @@ toggle.addEventListener("click", function (e) {
         return;
     }
     if (!email.includes("@") || !email.includes(".")) {
-       alert("Please enter a valid email!");
+        alert("Please enter a valid email!");
         return;
-   }
+    }
     if (isNaN(number)) {
         alert("Phone number must be numbers only!");
         return;
     }
-     alert("Message sent successfully!");
-}
-const menuBtn = document.getElementById("menuToggle");
-const nav = document.querySelector(".nav");
-const parent = document.querySelector(".perant");
-menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    parent.classList.toggle("menu-open");
-});
+    alert("Message sent successfully!");
+};
