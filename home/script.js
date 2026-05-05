c30492ba5f432f226284c52645c9562481700040
hla-alaa <hlaalaa07@gmail.com>
2026-05-05 22:59:55 +0300

    h

--- home/script.js
+++ home/script.js
@@ -1,10 +1,3 @@
-const toggle = document.getElementById("themeToggle");
-
-toggle.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  document.body.classList.toggle("dark-mode");
-});
 
 const btn = document.getElementById("layoutToggle");
 const body = document.getElementById("body");
